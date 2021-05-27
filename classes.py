dd69a10d7371801618228f3ef7c09dec931c2c42
mariamawuena <mari543k@stud.kea.dk>
2021-05-27 11:41:29 +0200

    cm2

---
+++ classes.py
@@ -0,0 +1,63 @@
+from functions import *
+import csv
+
+
+class Person():
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+
+class Coordinator(Person):
+    def __int__(self, first_name, last_name, user_id, password):
+        self.user_id = user_id
+        self.password = password
+        super().__init__(first_name, last_name)
+
+
+class Crew(Person):
+    def __int__(self, first_name, last_name, crew_id, job_id):
+        self.crew_id = crew_id
+        self.job_id = job_id
+        super().__init__(first_name, last_name)
+
+
+class Windco():
+    def __init__(self, windfarm_list):
+        self.windfarm_list = windfarm_list
+
+    def fetch_user(self, user_id, password):
+        pass
+
+    def fetch_all_windfarms(self):
+        pass
+
+    def fetch_crew(self, crew_id):
+        pass
+
+
+class Windfarm():
+    def __init__(self, windfarm_id, location, windmill_list, maintenance_total=0, status="OK"):
+        self.windfarm_id = windfarm_id
+        self.location = location
+        self.windmill_list = windmill_list
+        self.maintenance_total = maintenance_total
+        self.status = status
+
+    def fetch_all_windmills(self):
+        pass
+
+
+class Windmill():
+    def __init__(self, windmill_id, maintenance=False):
+        self.windmill_id = windmill_id
+        self.maintenance = maintenance
+
+
+class Maintenance_Job_Calendar():
+    def __init__(self, windfarm_id, job_role, date, time=0, status="OK"):
+        self.windfarm_id = windfarm_id
+        self.job_role = job_role
+        self.date = date
+        self.time = time
+        self.status = status
--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+"""
+Code laws:
+    1. Følg pycharms automatiske indentering
+    2. Klasser skrives med CamelCase
+    3. Navn for variabler opdeles med _
+    4. Navn for funktioner og metoder opdeles med _
+    5. Alle String skrives med "" citationstegn
+    6. Operatører skrives med mellemrum før og efter tegnet
+    7. Selve programmet skrives på dansk - kode altid på engelsk
+    8. Forklar koden med kommentarer løbende
+    9. Pas på med for lidt/for mange linjeskift både indeni scope og udenfor scope
+    10. Ved indlejring af variabler i en String benyttes format() metoden = "{}".format()
+"""
+
+from functions import *
+
 print("hello world!")
