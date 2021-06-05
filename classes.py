9ed296936982af44ea4100c0b283897f897df59d
mariamawuena <mari543k@stud.kea.dk>
2021-06-05 02:31:52 +0200

    Commit

--- classes.py
+++ classes.py
@@ -25,7 +25,61 @@ class Crew():
     def __init__(self, crew_id, job_id):
         self.crew_id = crew_id
         self.job_id = job_id
-        super().__init__(first_name, last_name)
+
+
+class Windfarm():
+    def __init__(self, windfarm_id, name, location, status):
+        self.windfarm_id = windfarm_id
+        self.name = name
+        self.location = location
+        self.status = status
+
+    def fetch_all_windmills(self):
+        maintenance_count = 0       # tælleren er som standard 0
+
+        with open("csv_windmills.csv", mode="r") as f:      # kigger ned i csv_windfarms fil
+            csv_windmills = csv.reader(f, delimiter=",")
+
+            print("\t\t\t\tVindmølle ID\tVedligeholdelse\t\tMegawatt".upper())      # danner tabel header
+
+            for row in csv_windmills:
+                if self.windfarm_id in row[3]:      # udvælger de vindmøller der indeholder værdien for windfarm_id i index 3
+
+                    if "True" in row:       # læg 1 til maintenance_count hvis True eksisterer for vindmøllerækken
+                        maintenance_count += 1
+
+                    windmill = Windmill(row[0], row[1], row[2], row[3])     # instansiering af Windmill objekt
+
+                    if windmill.maintenance == "True":      # omskriver boolean til forståelig tekst
+                        windmill.maintenance = "Ja"
+                    elif windmill.maintenance == "False":
+                        windmill.maintenance = "Nej"
+
+                    print("\t\t\t\t{}\t\t\t\t{}\t\t\t\t\t{}".format(windmill.windmill_id, windmill.maintenance,
+                                                                    windmill.megawatt))     # printer hver række
+
+        if maintenance_count == 0:      # alt afhængig af hvad der er sket med tælleren så print følgende
+            print("\n\t\t\t\tIngen vedligeholdelse påkrævet")
+        else:
+            print("\n\t\t\t\t{} vindmølle(r) skal vedligeholdes!".format(maintenance_count))
+
+
+class Windmill():
+    def __init__(self, windmill_id, maintenance, megawatt, windfarm_id):
+        self.windmill_id = windmill_id
+        self.maintenance = maintenance
+        self.megawatt = megawatt
+        self.windfarm_id = windfarm_id
+
+
+class Maintenance_Job_Calendar():
+    def __init__(self, windfarm_id, job_role, date, time, status, responsible):
+        self.windfarm_id = windfarm_id
+        self.job_role = job_role
+        self.date = date
+        self.time = time
+        self.status = status
+        self.responsible = responsible
 
 
 class Windco():
